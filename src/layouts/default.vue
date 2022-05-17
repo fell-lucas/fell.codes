f4c7b170d228985a058303ebb72d65dc0c56284a
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2022-05-16 22:06:33 -0300

    feat: removes boilerplate, adds new content

--- src/layouts/default.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <main class="px-4 py-6 text-center text-gray-700 dark:text-gray-200">
-    <Nav class="mb-18" />
-    <RouterView />
-  </main>
-</template>
--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 const { t } = useI18n()
+
+useHead({
+  title: t('not-found'),
+})
 </script>
 
 <template>
-  <div>
-    {{ t('not-found') }}
+  <div class="prose prose-sm m-auto text-left">
+    <h4 text="[#ea580c] lg opacity-100" font="medium" class="not-prose">
+      404
+    </h4>
+    <h1>
+      {{ t('not-found') }}
+    </h1>
+    <p>{{ t('not-found-msg') }}</p>
   </div>
 </template>
-
-<route lang="yaml">
-meta:
-  layout: 404
-</route>
